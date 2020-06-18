a1b4cc53a732ea5471eb7ce79a40374ec6ce7884
Raul Hernandez <raulhernandez@Rauls-MacBook-Pro.local>
2020-06-18 14:24:35 -0500

    feature: SingleCharacter on AppRouter

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import RickAndMortyApp from '../components/RickAndMortyApp'
-import CharacterDetail from '../components/CharacterDetail'
+import SingelCharacter from '../components/SingleCharacter'
 
 const AppRouter = () => (
     <BrowserRouter>
       <div>
         <Switch>
           <Route path="/" component={RickAndMortyApp} exact={true} />
-          <Route path="/detail/:id" component={CharacterDetail} />
+          <Route path="/detail/:id" component={SingelCharacter} />
         </Switch>
       </div>
     </BrowserRouter>
