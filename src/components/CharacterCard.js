6c21ad555dd464f8b2833b64b36af6bc98d36cb6
Raul Hernandez <raulhernandez@Rauls-MacBook-Pro.local>
2020-06-17 23:14:22 -0500

    feature: CharacterCard component set

---
+++ src/components/CharacterCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CharacterCard = () => {
+    return (
+        <div>
+        This is the character card component
+        </div>
+    )
+}
+
+export default CharacterCard
