25156fb38a9eec8b178c94eb47a37b1804fa1ba0
Raul Hernandez <raulhernandez@Rauls-MacBook-Pro.local>
2020-06-18 00:11:33 -0500

    feature: CharacterDetail render

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-const CharacterDetail = () => {
+const CharacterDetail = (props) => {
+    const [character, setCharacter] = useState('')
+    const id = props.match.params.id
+
+    const charLoad = () => {
+        axios.get(`https://rickandmortyapi.com/api/character/${id}`)
+        .then(res => {
+            setCharacter(res.data)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+
+    useEffect(() => {
+        charLoad()
+    }, []) //eslint-disable-line
+
       return(
           <div>
-          Character Detail component
+            <img src={character.image} alt={character.id}/>
+            <h2>Name: {character.name}</h2>
+            <h4>Status: {character.status}</h4>
+            <h4>Species: {character.species} </h4>
+            <h4>Gender: {character.gender}</h4>
+            <h4>Origin: {character.origin.name}</h4>
+            <h4>Last seen on: {character.location.name}</h4>
           </div>
       )
 }
