74ee42bafd639678f3ab6bef5199949ae07bb1c0
Raul Hernandez <raulhernandez@Rauls-MacBook-Pro.local>
2020-06-17 23:18:38 -0500

    feature: CharacterList component configuration

---
+++ src/components/CharacterDetail.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CharacterList = () => {
+      return(
+          <div>
+          Character list component
+          </div>
+      )
+}
+
+export default CharacterList
