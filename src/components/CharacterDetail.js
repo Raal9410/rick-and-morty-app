2f0d1d45d31fe0d60df91275fdd0cf61bc5e190b
Raul Hernandez <raulhernandez@Rauls-MacBook-Pro.local>
2020-06-18 00:39:17 -0500

    feature: go back button on detail

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -4,11 +4,11 @@ import axios from 'axios'
 const CharacterDetail = (props) => {
     const [character, setCharacter] = useState('')
     const id = props.match.params.id
-
     const charLoad = () => {
         axios.get(`https://rickandmortyapi.com/api/character/${id}`)
         .then(res => {
             setCharacter(res.data)
+            console.log({...res.data})
         })
         .catch(err => {
             console.log(err)
@@ -16,18 +16,23 @@ const CharacterDetail = (props) => {
     }
 
     useEffect(() => {
+        console.log(character)
         charLoad()
     }, []) //eslint-disable-line
 
+    const goBack = () => {
+        props.history.push('/')
+    }
+
       return(
           <div>
+          <button onClick={goBack}>Go Back</button>
             <img src={character.image} alt={character.id}/>
             <h2>Name: {character.name}</h2>
             <h4>Status: {character.status}</h4>
             <h4>Species: {character.species} </h4>
             <h4>Gender: {character.gender}</h4>
-            <h4>Origin: {character.origin.name}</h4>
-            <h4>Last seen on: {character.location.name}</h4>
+            {/* <h4>Gender: {character.origin.name}</h4> */}
           </div>
       )
 }
