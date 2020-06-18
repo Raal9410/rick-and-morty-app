6cf9173c34dc379cd0997feb62f278a35b6f04f2
Raul Hernandez <raulhernandez@Rauls-MacBook-Pro.local>
2020-06-18 14:22:54 -0500

    feature: SingleCharacter View component

---
+++ src/components/SingleCharacter.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import {useRickAndMortyAPI, Context} from '../context/context'
+
+const SingleCharacterView = () => {
+    const data = useRickAndMortyAPI()
+    return(
+        <div>
+            <Context.Provider value={data}>
+                Character View
+            </Context.Provider>
+        </div>
+    )
+}
