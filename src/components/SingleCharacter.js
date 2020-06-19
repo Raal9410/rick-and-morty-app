eef249f9b58746a64ebca4650d955e271dc6ae79
Raul Hernandez <raulhernandez@Rauls-MacBook-Pro.local>
2020-06-19 09:14:00 -0500

    feature: backgound, styled button

--- src/components/SingleCharacter.js
+++ src/components/SingleCharacter.js
@@ -1,6 +1,45 @@
 import React, {useContext, useEffect} from 'react'
 import { Context } from '../context/context'
 import CharacterDetail from './CharacterDetail'
+import styled from 'styled-components'
+
+const DetailContainer = styled.div `
+    background-color:#EDE7F6;
+    height: 96vh;
+    width: 99vw;
+    margin: 0
+    @media(min-width: 600px){
+        background-color:#EDE7F6;
+        height: 96vh;
+        width: 99vw;
+        margin: 0
+    }
+`
+const Button = styled.button `
+  display: inline-block;
+  color:  #32bcd0;
+  font-size: .8rem;
+  width: 8rem;
+  padding: 0.25rem 1rem;
+  border: 2px solid #aaec4d;
+  margin-top: 0.5rem
+  border-radius: 3px;
+  margin-left: 10px;
+  @media(min-width: 600px){
+    &:hover{
+    box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
+  };
+    display: inline-block;
+    color:  #32bcd0;
+    font-size: 1rem;
+    width: 10rem;
+    padding: 0.25rem 1rem;
+    border: 2px solid #aaec4d;
+    margin-top: 0.5rem
+    border-radius: 3px;
+    margin-left: 10px;
+  }
+`
 
 const SingleCharacterView = (props) => {
     const id = props.match.params.id
@@ -15,10 +54,10 @@ const SingleCharacterView = (props) => {
     }, []) //eslint-disable-line
 
     return(
-        <div>
-        <button onClick={goBack}> Go Back </button>
+        <DetailContainer>
+        <Button onClick={goBack}> Go Back </Button>
         <CharacterDetail character={singleCharacter}/>
-        </div>
+        </DetailContainer>
     )
 }
 
