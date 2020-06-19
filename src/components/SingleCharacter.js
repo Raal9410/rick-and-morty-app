820dd48a4205d83187175d34f6ecb61eeae04dff
Raul Hernandez <raulhernandez@Rauls-MacBook-Pro.local>
2020-06-18 19:47:10 -0500

    feature: goBack button

--- src/components/SingleCharacter.js
+++ src/components/SingleCharacter.js
@@ -6,13 +6,17 @@ const SingleCharacterView = (props) => {
     const id = props.match.params.id
     const {singleCharacter, fetchOneCharacter} = useContext(Context)
 
+    const goBack = () => {
+        props.history.goBack()
+    }
+
     useEffect(() => {
         fetchOneCharacter(id)
-        console.log(singleCharacter)
     }, []) //eslint-disable-line
 
     return(
         <div>
+        <button onClick={goBack}> Go Back </button>
         <CharacterDetail character={singleCharacter}/>
         </div>
     )
